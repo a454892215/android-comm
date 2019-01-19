3f3c20ae3aa23de02ed4b217fc8a61047a19fabc
LiuPan <1573802540@qq.com>
2019-01-19 11:12:42 +0800

    升级studio到3.3以解决R文件不能找到 与代码优化等

--- app/src/main/java/com/test/util/XposedTestActivity.java
+++ app/src/main/java/com/test/util/XposedTestActivity.java
@@ -1,14 +1,18 @@
 package com.test.util;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.common.base.BaseActivity;
+import com.common.utils.ToastUtil;
 
 public class XposedTestActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(v -> ToastUtil.showLong(activity, "我是button"));
     }
 
     @Override
