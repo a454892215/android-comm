ad9908fdaf00350a29538ec17b1acfd385bbbf57
L <unkown@.com>
2019-03-29 17:27:42 +0800

    动画

--- common/src/main/java/com/common/dialog/CenterDialogFragment.java
+++ common/src/main/java/com/common/dialog/CenterDialogFragment.java
@@ -1,11 +1,18 @@
 package com.common.dialog;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.View;
 
 import com.common.R;
 import com.common.base.BaseDialogFragment;
 
 public class CenterDialogFragment extends BaseDialogFragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setAnimStyle(R.style.dialog_anim_center);
+    }
 
     @Override
     protected int getLayoutId() {
