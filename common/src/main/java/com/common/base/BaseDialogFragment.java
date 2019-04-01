9a964ffa7f17a6a4655268dc1b927c9f8cc1aab3
L <unkown@.com>
2019-04-01 14:16:48 +0800

    补

--- common/src/main/java/com/common/base/BaseDialogFragment.java
+++ common/src/main/java/com/common/base/BaseDialogFragment.java
@@ -54,6 +54,7 @@ public abstract class BaseDialogFragment extends DialogFragment {
         //  getDialog().setOnKeyListener((dialog, keyCode, event) -> keyCode == KeyEvent.KEYCODE_BACK);
         if (window != null) {
             window.setBackgroundDrawableResource(android.R.color.transparent);
+            window.getDecorView().setPadding(0, 0, 0, 0);
             WindowManager.LayoutParams lp = window.getAttributes();
             lp.gravity = gravity;
             lp.width = width;
