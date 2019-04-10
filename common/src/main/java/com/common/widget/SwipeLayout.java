2d00935ed9e14bca574b876e8ab09c22240a8fd5
L <unkown@.com>
2019-04-10 11:32:30 +0800

    删除冗余代码

--- common/src/main/java/com/common/widget/SwipeLayout.java
+++ common/src/main/java/com/common/widget/SwipeLayout.java
@@ -9,7 +9,6 @@ import android.view.ViewGroup;
 import android.widget.HorizontalScrollView;
 
 import com.common.R;
-import com.common.utils.DensityUtils;
 
 public class SwipeLayout extends HorizontalScrollView {
 
