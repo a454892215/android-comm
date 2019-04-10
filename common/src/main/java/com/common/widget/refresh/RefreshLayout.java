c264354f8f880b45c111a5b5b380030be634d409
L <unkown@.com>
2019-04-10 17:35:46 +0800

    refresh上下处理事件时候，事件不再分发

--- common/src/main/java/com/common/widget/refresh/RefreshLayout.java
+++ common/src/main/java/com/common/widget/refresh/RefreshLayout.java
@@ -244,7 +244,7 @@ public class RefreshLayout extends LinearLayout {
                 break;
         }
         boolean consume = true;
-        if (!isIntercept) consume = super.dispatchTouchEvent(ev);
+        if (!isIntercept && Math.abs(getScrollY())< min_scroll_unit*10) consume = super.dispatchTouchEvent(ev);
         return consume;
     }
 
