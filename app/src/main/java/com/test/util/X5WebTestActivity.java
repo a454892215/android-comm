fd445dbafc93e89189af514a7a6ccb1c59dd9763
L <unkown@.com>
2019-04-24 11:05:21 +0800

    删除冗余

--- app/src/main/java/com/test/util/X5WebTestActivity.java
+++ app/src/main/java/com/test/util/X5WebTestActivity.java
@@ -4,8 +4,6 @@ import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ProgressBar;
 
