9c1d2c9cf4581c0079cb262d2c8ce2e0fef528c4
L <unkown@.com>
2019-04-25 18:56:08 +0800

    glide 配置

---
+++ common/src/main/java/com/common/MyAppGlideModule.java
@@ -0,0 +1,9 @@
+package com.common;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule {
+}
+
