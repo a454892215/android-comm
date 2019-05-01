2fc91f3373a596ae4f47bc6d68a7480dfd40d600
L <unkown@.com>
2019-05-01 18:06:09 +0800

    --

--- common/src/main/java/com/common/widget/trend/TrendChartView.java
+++ common/src/main/java/com/common/widget/trend/TrendChartView.java
@@ -114,13 +114,11 @@ public class TrendChartView extends View {
         maxCanScrollWidth = maxCanScrollWidth > 0 ? maxCanScrollWidth : 0;
         LogUtil.d("===================maxCanScrollWidth:" + maxCanScrollWidth + "  xUnitLength:" + xUnitLength);
         trendPath.reset();
-        trendPath.addArc(200, 200, 200 + 200, 400, 0, 359.9f);
-        trendPath.arcTo(400, 200, 400 + 200, 400, 180, 359.9f,false);
+        // trendPath.addArc(200, 200, 200 + 200, 400, 0, 359.9f);
+        //  trendPath.arcTo(400, 200, 400 + 200, 400, 180, 359.9f,false);
         //trendPath.close();
-        //  trendPath.lineTo(400, 542);
-        //  trendPath.close();
-        //  trendPath.rQuadTo(dp_1 * 180, dp_1 * 110,dp_1 * 0, dp_1 * 50);//第一点位控制点，第二终点
-/*        for (int i = 0; i < size; i++) {
+
+        for (int i = 0; i < size; i++) {
             Point point_1 = list.get(i);
             float x_dp_p1;
             if (i == 0) {
@@ -135,8 +133,7 @@ public class TrendChartView extends View {
             } else {
                 trendPath.lineTo(x_dp_p1, y_dp_p1);
             }
-            //  trendPath.addCircle(x_dp_p1, y_dp_p1, joinRadius, Path.Direction.CW);
-        }*/
+        }
         startAnimDraw();
     }
 
