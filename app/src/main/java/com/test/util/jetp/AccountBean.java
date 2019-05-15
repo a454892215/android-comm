fd74d9688ec13e4eda1ec0d8e2cc51dd10603cea
L <unkown@.com>
2019-05-15 09:36:00 +0800

    git命令补充等

---
+++ app/src/main/java/com/test/util/jetp/AccountBean.java
@@ -0,0 +1,4 @@
+package com.test.util.jetp;
+
+public class AccountBean {
+}
---
+++ app/src/main/java/com/test/util/jetp/AccountModel.java
@@ -0,0 +1,28 @@
+package com.test.util.jetp;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.MutableLiveData;
+import android.support.annotation.NonNull;
+
+public class AccountModel extends AndroidViewModel {
+    // 创建LiveData
+    private MutableLiveData<AccountBean> mAccount = new MutableLiveData<>();
+
+    public MutableLiveData<AccountBean> getmAccount() {
+        return mAccount;
+    }
+
+    public void setmAccount(MutableLiveData<AccountBean> mAccount) {
+        this.mAccount = mAccount;
+    }
+
+    public AccountModel(@NonNull Application application) {
+        super(application);
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+    }
+}
