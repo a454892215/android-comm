c9efddeae8de8fe3804ec601c5f98a0e31d90c4b
android2 <android2@s.com>
2019-06-10 16:03:22 +0800

    1.add  Toast function。 2.import rxJava repository。

--- common/src/main/java/com/common/utils/ToastUtil.java
+++ common/src/main/java/com/common/utils/ToastUtil.java
@@ -2,7 +2,12 @@ package com.common.utils;
 
 import android.content.Context;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.common.R;
 
 /**
  * Description: Toast工具类
@@ -20,6 +25,23 @@ public class ToastUtil {
     public static void showLong(Context context, CharSequence message) {
         Toast toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
         toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+    }
+
+    public static void show(Context context, String msg) {
+        Toast toast = new Toast(context);
+        //设置Toast显示位置，居中，向 X、Y轴偏移量均为0
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        //获取自定义视图
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_toast, null);
+        TextView tvMessage = (TextView) view.findViewById(R.id.layout_toast_text);
+        //设置文本
+        tvMessage.setText(msg);
+        //设置视图
+        toast.setView(view);
+        //设置显示时长
+        toast.setDuration(Toast.LENGTH_SHORT);
+        //显示
         toast.show();
     }
 }
