fb4d80d780f48dfef56c10c88f833a0cc6c3b330
L <unkown.com>
2019-06-17 09:48:51 +0800

    轮播图补

--- app/src/main/java/com/test/util/custom_view/rv/RVTest4Fragment.java
+++ app/src/main/java/com/test/util/custom_view/rv/RVTest4Fragment.java
@@ -10,8 +10,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.common.base.BaseFragment;
 import com.common.helper.RVHelper;
-import com.common.test.TestEntity;
-import com.common.utils.LogUtil;
 import com.common.utils.MathUtil;
 import com.common.widget.refresh.RefreshLayout;
 import com.test.util.R;
@@ -34,10 +32,6 @@ public class RVTest4Fragment extends BaseFragment {
         rv.smoothScrollToPosition(Integer.MAX_VALUE / 2 + 2);
         RefreshLayout refresh_layout = findViewById(R.id.refresh_layout);
         refresh_layout.setInterceptEventOnScrolled(true);
-        refresh_layout.setOnRefreshListener(refreshLayout -> refresh_layout.postDelayed(() -> {
-            refresh_layout.notifyLoadFinish();
-            RVHelper.notifyAdapterRefresh(TestEntity.getList(), rv);
-        }, 800));
         rv.addOnScrollListener(new RecyclerView.OnScrollListener() {
             private float leftOfCenterView;
             private float leftOfRightView;
@@ -50,14 +44,14 @@ public class RVTest4Fragment extends BaseFragment {
                 if (leftOfCenterView == 0 && isHasIDLE && newState == RecyclerView.SCROLL_STATE_IDLE) {
                     leftOfCenterView = recyclerView.getChildAt(1).getLeft();
                     leftOfRightView = recyclerView.getChildAt(2).getLeft();
-                    LogUtil.d("=====onScrollStateChanged:" + "  leftOfCenterView:" + leftOfCenterView + "  leftOfRightView:" + leftOfRightView);
+                    onScrolled(recyclerView, 0, 0);
+                   // LogUtil.d("=====onScrollStateChanged:" + "  leftOfCenterView:" + leftOfCenterView + "  leftOfRightView:" + leftOfRightView);
                 }
                 if (newState == RecyclerView.SCROLL_STATE_IDLE) isHasIDLE = true;
             }
 
             @Override
             public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
                 if (leftOfCenterView != 0) {
                     int childCount = recyclerView.getChildCount();
                     for (int i = 0; i < childCount; i++) {
@@ -66,8 +60,8 @@ public class RVTest4Fragment extends BaseFragment {
                         //靠近率 为0时，最靠近。值域[-1,1]
                         float kjl = MathUtil.clamp(kjd / (leftOfRightView - leftOfCenterView), -1, 1);
                         float scale = 1 - Math.abs(kjl); //缩放 [1,0]
-                        scale = (scale + 9f)/10f;// [1,0] - > [1,0.9]
-                        scale = MathUtil.clamp(scale,0f,1f);
+                        scale = (scale + 9f) / 10f;// [1,0] - > [1,0.9]
+                        scale = MathUtil.clamp(scale, 0f, 1f);
                         if (child instanceof ViewGroup) {
                             View imgView = ((ViewGroup) child).getChildAt(0);
                             if (imgView instanceof ImageView) {
@@ -76,9 +70,9 @@ public class RVTest4Fragment extends BaseFragment {
                             }
                         }
 
-                        LogUtil.d("================kjd =:" + kjd + "  kjl:" + kjl + "  scale:" + scale);
+                      //  LogUtil.d("================kjd =:" + kjd + "  kjl:" + kjl + "  scale:" + scale);
                     }
-                    LogUtil.d("=======================================>>>>>>:" + (activity.dp_1 * 280));
+                  //  LogUtil.d("=======================================>>>>>>:" + (activity.dp_1 * 280));
                 }
 
             }
