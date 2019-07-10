c14deb395cd9f99d47f3efeaa620106a94613098
L <unkown.com>
2019-07-10 16:29:46 +0800

    优化

--- common/src/main/java/com/common/utils/ToastUtil.java
+++ common/src/main/java/com/common/utils/ToastUtil.java
@@ -1,5 +1,6 @@
 package com.common.utils;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Looper;
 import android.view.Gravity;
@@ -12,71 +13,46 @@ import com.common.R;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
 
 
 /**
  * Description: Toast工具类
  */
-
+@SuppressLint("CheckResult")
 public class ToastUtil {
 
-    public static boolean isMainThread() {
-        return Looper.getMainLooper() == Looper.myLooper();
+    private static boolean isNotMainThread() {
+        return Looper.getMainLooper() != Looper.myLooper();
     }
 
+
     public static void showShort(Context context, CharSequence message) {
-        if (!isMainThread()) {
-            Observable.just(1).observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(integer -> {
-                        showShortToast(context, message);
-                    });
+        if (isNotMainThread()) {
+            CompositeDisposable compositeDisposable = new CompositeDisposable();
+            Disposable subscribe = Observable.just(1).observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(integer -> showShortToast(context, message));
+            compositeDisposable.add(subscribe);
             return;
         }
         showShortToast(context, message);
     }
 
-    public static void showShort(Context context, int resId) {
-        if (!isMainThread()) {
-            Observable.just(1).observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(integer -> {
-                        showShortToast(context, resId);
-                    });
-            return;
-        }
-        showShortToast(context, resId);
-    }
-
 
     public static void showLong(Context context, CharSequence message) {
-        if (!isMainThread()) {
-            Observable.just(1).observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(integer -> {
-                        showLongToast(context, message);
-                    });
+        if (isNotMainThread()) {
+            CompositeDisposable compositeDisposable = new CompositeDisposable();
+            Disposable subscribe = Observable.just(1).observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(integer -> showLongToast(context, message));
+            compositeDisposable.add(subscribe);
             return;
         }
         showLongToast(context, message);
     }
 
-    public static void showLong(Context context, int resId) {
-        if (!isMainThread()) {
-            Observable.just(1).observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(integer -> {
-                        showLongToast(context, resId);
-                    });
-            return;
-        }
-        showLongToast(context, resId);
-    }
-
     private static void showShortToast(Context context, CharSequence message) {
         Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.CENTER, 0, 0);
-        toast.show();
-    }
-
-    private static void showShortToast(Context context, int resId) {
-        Toast toast = Toast.makeText(context, resId, Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER, 0, 0);
         toast.show();
     }
@@ -87,19 +63,14 @@ public class ToastUtil {
         toast.show();
     }
 
-    private static void showLongToast(Context context, int resId) {
-        Toast toast = Toast.makeText(context, resId, Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.CENTER, 0, 0);
-        toast.show();
-    }
 
     public static void show(Context context, String msg) {
         Toast toast = new Toast(context);
         //设置Toast显示位置，居中，向 X、Y轴偏移量均为0
         toast.setGravity(Gravity.CENTER, 0, 0);
         //获取自定义视图
-        View view = LayoutInflater.from(context).inflate(R.layout.layout_toast, null);
-        TextView tvMessage = (TextView) view.findViewById(R.id.layout_toast_text);
+        @SuppressLint("InflateParams") View view = LayoutInflater.from(context).inflate(R.layout.layout_toast, null);
+        TextView tvMessage = view.findViewById(R.id.layout_toast_text);
         //设置文本
         tvMessage.setText(msg);
         //设置视图
