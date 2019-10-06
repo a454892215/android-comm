6c43dc28a782593b14eb60f6f5e1ba7cb9d84e66
LiuPan <1573802540@qq.com>
2019-10-06 21:55:32 +0800

    升级类库等

--- common/src/main/java/com/common/comm/L.java
+++ common/src/main/java/com/common/comm/L.java
@@ -1,9 +1,18 @@
 package com.common.comm;
 
+import android.content.Context;
+
+import com.common.R;
+
 /**
  * Author: Pan
  * 2019/10/6
  * Description:
  */
 public class L {
+    public static float dp_1;
+
+    public static void init(Context context) {
+        dp_1 = context.getResources().getDimension(R.dimen.dp_1);
+    }
 }
