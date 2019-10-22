c9153c0ada17592abc36651ed91a31b554916a99
L <unkown.com>
2019-10-22 09:57:59 +0800

    bannerUtil

--- common/src/main/java/com/common/widget/banner/BannerUtil.java
+++ common/src/main/java/com/common/widget/banner/BannerUtil.java
@@ -1,7 +1,11 @@
 package com.common.widget.banner;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+
+import androidx.viewpager.widget.ViewPager;
 
 import com.bumptech.glide.Glide;
 import com.youth.banner.Banner;
@@ -24,5 +28,30 @@ public class BannerUtil {
         banner.setImages(urlList);
         //banner设置方法全部调用完毕时最后调用
         banner.start();
+    }
+
+    public static void setIndicator(Banner banner, ViewGroup parent, int indicatorLayoutId, int indicatorCount) {
+        for (int i = 0; i < indicatorCount; i++) {
+            LayoutInflater.from(parent.getContext()).inflate(indicatorLayoutId, parent, true);
+        }
+        banner.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for (int i = 0; i < indicatorCount; i++) {
+                    parent.getChildAt(i).setSelected(false);
+                }
+                parent.getChildAt(position).setSelected(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
