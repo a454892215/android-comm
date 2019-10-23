c52d6a0d9d3e186e93af253e385968504149253e
L <unkown@.com>
2019-10-23 09:23:59 +0800

    删除旧版chart 准备导入新版chart

--- app/src/main/java/com/test/util/custom_view/fragment/TrendChartFragment_1.java
+++ app/src/main/java/com/test/util/custom_view/fragment/TrendChartFragment_1.java
@@ -1,23 +1,12 @@
 package com.test.util.custom_view.fragment;
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
+
 
 import com.common.base.BaseFragment;
-import com.common.utils.CastUtil;
-import com.common.utils.InstanceUtil;
-import com.common.widget.CommonTabLayout;
-import com.common.widget.chart.DataHandler_temple;
-import com.common.widget.chart.DataHandler_temple_2;
-import com.common.widget.chart.IDataHandler;
-import com.common.widget.chart.RowCell;
-import com.common.widget.chart.widget.CustomTableView;
-import com.common.widget.refresh.RefreshLayout;
+
+
 import com.test.util.R;
 
-import java.util.List;
 
 public class TrendChartFragment_1 extends BaseFragment {
 
@@ -28,7 +17,7 @@ public class TrendChartFragment_1 extends BaseFragment {
 
     @Override
     protected void initView() {
-        String[] tabNames = {"组合走势图1", "组合走势图2", "组合走势图3", "组合走势图4"};
+      /*  String[] tabNames = {"组合走势图1", "组合走势图2", "组合走势图3", "组合走势图4"};
         Class dataHandlerArr[] = {DataHandler_temple.class, DataHandler_temple_2.class, DataHandler_temple.class, DataHandler_temple_2.class,};
         FrameLayout flt_content = findViewById(R.id.flt_content);
         CommonTabLayout tab_layout_2 = findViewById(R.id.tab_layout_2);
@@ -39,12 +28,12 @@ public class TrendChartFragment_1 extends BaseFragment {
                 setTableView(dataHandler, flt_content);
             }
         });
-        tab_layout_2.setCurrentPosition(0);
+        tab_layout_2.setCurrentPosition(0);*/
 
 
     }
 
-    private void setTableView(IDataHandler dataHandler, ViewGroup view_content) {
+/*    private void setTableView(IDataHandler dataHandler, ViewGroup view_content) {
         view_content.removeAllViews();
         LayoutInflater.from(activity).inflate(R.layout.layout_refresh_custom_table, view_content, true);
         CustomTableView table_view = view_content.findViewById(R.id.table_view);
@@ -55,5 +44,5 @@ public class TrendChartFragment_1 extends BaseFragment {
         List<List<RowCell>> lists = dataHandler.handleData();
         table_view.setHeaderData(lists.get(0), lists.get(1));
         table_view.setBodyData(lists.get(2), lists.get(3));
-    }
+    }*/
 }
