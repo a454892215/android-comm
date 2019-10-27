418ddef12c873b5e87c856c430b751f3b92c003c
L <unkown@.com>
2019-10-27 18:49:45 +0800

    --

---
+++ common/src/main/java/com/common/widget/comm/EditTextUtil.java
@@ -0,0 +1,36 @@
+package com.common.widget.comm;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EditTextUtil {
+
+    public static void addWordNumFilter(EditText editText) {
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String editable = editText.getText().toString();
+                String regEx = "[^a-zA-Z0-9]";  //只能输入字母或数字
+                Pattern p = Pattern.compile(regEx);
+                Matcher m = p.matcher(editable);
+                String str = m.replaceAll("").trim();    //删掉不是字母或数字的字符
+                if (!editable.equals(str)) {
+                    editText.setText(str);  //设置EditText的字符
+                    editText.setSelection(str.length()); //因为删除了字符，要重写设置新的光标所在位置
+                }
+            }
+        });
+    }
+}
