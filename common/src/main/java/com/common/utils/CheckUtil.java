8366ac1380f88202ff834664af584c7bf0838a6e
L <unkown@.com>
2019-10-28 16:09:36 +0800

    工具类补充

---
+++ common/src/main/java/com/common/utils/CheckUtil.java
@@ -0,0 +1,27 @@
+package com.common.utils;
+
+import android.content.Context;
+
+public class CheckUtil {
+
+    public static boolean checkAmount(String currentAmount, String min, String max, Context context) {
+        min = StringUtil.isFloat(min) ? min : "0";
+        max = StringUtil.isFloat(max) ? max : "1000000";
+        float minAmount = Float.parseFloat(min);
+        float maxAmount = Float.parseFloat(max);
+        if (StringUtil.isFloat(currentAmount)) {
+            float amount_f = Float.parseFloat(currentAmount);
+            if (amount_f < minAmount) {
+                ToastUtil.showShort(context, "金额不能小于" + min + "元");
+            } else if (amount_f > maxAmount) {
+                ToastUtil.showShort(context, "金额不能大于" + max + "元");
+            } else {
+                return true;
+            }
+        } else {
+            ToastUtil.showShort(context, "请输入合法金额");
+        }
+        return false;
+    }
+
+}
--- common/src/main/java/com/common/utils/MathUtil.java
+++ common/src/main/java/com/common/utils/MathUtil.java
@@ -1,5 +1,7 @@
 package com.common.utils;
 
+import java.math.BigDecimal;
+
 /**
  * Author:  L
  * Description: No
@@ -29,5 +31,17 @@ public class MathUtil {
             num = num > -min ? -min : num;
         }
         return num;
+    }
+
+    public static float mul(float... values) {
+        BigDecimal total = new BigDecimal(String.valueOf(1));
+        for (float value : values) {
+            total = total.multiply(new BigDecimal(String.valueOf(value)));
+        }
+        return total.floatValue();
+    }
+
+    public static float sub(float value1, float value2) {
+        return new BigDecimal(String.valueOf(value1)).subtract(new BigDecimal(String.valueOf(value2))).floatValue();
     }
 }
