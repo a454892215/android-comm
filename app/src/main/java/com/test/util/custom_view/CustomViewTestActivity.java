b9b13af603cd9dcc5ccce238256f4dcf84e7bec2
L <unkown@.com>
2019-10-29 09:08:59 +0800

    progressBar 示例

--- app/src/main/java/com/test/util/custom_view/CustomViewTestActivity.java
+++ app/src/main/java/com/test/util/custom_view/CustomViewTestActivity.java
@@ -25,7 +25,7 @@ public class CustomViewTestActivity extends BaseAppActivity {
     Class[] fragmentArr = {TabFragment_01.class, CityPickerFragment_02.class, DialogTestFragment_03.class,
             TrendChartFragment_04.class, RVFragment_05.class, FloatWindowFragment_06.class
             , TestFragment_07.class, TestFragment_08.class};
-    String[] tabNames = {"Tab和速度", "Picker和TV", "弹窗", "Chart", "RV相关", "悬浮窗", "Transition", "InputLayout"};
+    String[] tabNames = {"Tab和速度", "Picker和TV", "弹窗和Progress", "Chart", "RV相关", "悬浮窗", "Transition", "InputLayout"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/test/util/custom_view/fragment/DialogTestFragment_03.java
+++ app/src/main/java/com/test/util/custom_view/fragment/DialogTestFragment_03.java
@@ -2,15 +2,18 @@ package com.test.util.custom_view.fragment;
 
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.common.base.BaseFragment;
+import com.common.comm.timer.MyCountDownTimer;
 import com.common.dialog.BottomDialogFragment;
 import com.common.dialog.CenterDialogFragment;
 import com.common.helper.FragmentHelper;
 import com.common.utils.CastUtil;
+import com.common.utils.FastClickUtil;
 import com.test.util.R;
 import com.test.util.custom_view.fragment.dialog.BottomDialogFragment_2;
 import com.test.util.custom_view.fragment.dialog.DropDialogFragment;
@@ -40,5 +43,16 @@ public class DialogTestFragment_03 extends BaseFragment {
             DialogFragment dialogFragment = (DialogFragment) FragmentHelper.getInstance(fm, CastUtil.cast(fragmentArr[finalI]));//缓存模式 无懒加载
             view.setOnClickListener(v -> dialogFragment.show(fm, fragmentArr[finalI].getName()));
         }
+
+        ProgressBar progress_bar = findViewById(R.id.progress_bar);
+        progress_bar.setProgress(100);
+
+        progress_bar.setOnClickListener(v -> {
+            FastClickUtil.isFastClick(3000);
+            MyCountDownTimer timer = new MyCountDownTimer(100, 30);
+            timer.setOnTickListener((time, count) -> progress_bar.setProgress(count));
+            timer.start();
+        });
     }
+
 }
