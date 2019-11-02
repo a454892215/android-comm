e8f0fe99f1e4027b212978d28e62b25900657097
L <unkown@.com>
2019-11-02 16:53:10 +0800

    soundpoolutil工具完善

--- common/src/main/java/com/common/utils/SoundPoolUtil.java
+++ common/src/main/java/com/common/utils/SoundPoolUtil.java
@@ -47,10 +47,16 @@ public class SoundPoolUtil {
     }
 
 
-    public void playOnlyOne(int resId, int playTime) {
+    /**
+     * 示例 soundPoolUtil.playOnlyOne(R.raw.music_activity, 4, activity)
+     * @param resId R.raw.music_aty
+     * @param playTime 4
+     * @param activity activity
+     */
+    public void playOnlyOne(int resId, int playTime, BaseActivity activity) {
         soundPool.setOnLoadCompleteListener((soundPool, sampleId, status) -> {
             int streamID = soundPool.play(sampleId, volume, volume, 1, 0, 1);
-           // activity.setOnPauseListener(() -> soundPool.stop(streamID));
+            activity.addOnPauseListener(() -> soundPool.stop(streamID));
             MyCountDownTimer timer = new MyCountDownTimer(playTime + 2, 1000);
             timer.setOnLastTickListener((time, count) -> soundPool.unload(sampleId));
             timer.start();
