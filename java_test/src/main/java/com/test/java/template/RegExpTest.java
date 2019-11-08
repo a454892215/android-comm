3c55c13425b2e5dde89403e7b21beb74c3b7e7ce
L <unkown@.com>
2019-11-08 14:41:27 +0800

    删除冗余代码

--- java_test/src/main/java/com/test/java/template/RegExpTest.java
+++ java_test/src/main/java/com/test/java/template/RegExpTest.java
@@ -1,5 +1,7 @@
 package com.test.java.template;
 
+
+
 import com.test.java.util.LogUtil;
 
 import java.util.regex.Matcher;
@@ -8,9 +10,10 @@ import java.util.regex.Pattern;
 public class RegExpTest {
 
     public static void main(String[] args) {
-        String regEx = "[^a-zA-Z0-9]|a|b|/.";  //对字母数字取反  外加 a,b,.
+      //  String regEx = "[^a-zA-Z0-9]|a|b|/.";  //对字母数字取反  外加 a,b,.
+        String regEx = "[^a-zA-Z0-9\\u4e00-\\u9fa5.]";  //对字母数字中文取反  外加.
         Pattern pattern = Pattern.compile(regEx);
-        Matcher matcher = pattern.matcher("abc你好吗d哈哈123.");
+        Matcher matcher = pattern.matcher("abc你好吗d哈哈123.%&^");
         while (matcher.find()) {
             String group = matcher.group();
             LogUtil.d(group);
