ed0634277014e51fc687c18bedb6a672aeb0f1b1
L <unkown@.com>
2019-11-12 09:52:07 +0800

    模板代码更新

--- common/src/main/java/com/common/template/Template1.java
+++ common/src/main/java/com/common/template/Template1.java
@@ -2,8 +2,10 @@ package com.common.template;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.webkit.WebView;
 import android.widget.ImageView;
 
@@ -19,10 +21,18 @@ public class Template1 {
     /**
      * WebView 加载html格式的文本
      */
-    public static void tem1(Context context) {
-        WebView web_view = new WebView(context);
+    public static void tem1(Activity activity) {
+        //加载html 格式文本
+        WebView web_view = new WebView(activity);
         web_view.setBackgroundColor(Color.parseColor("#000000")); //黑色背景
         web_view.loadDataWithBaseURL(null, "html格式的数据", "text/html", "UTF-8", null);
+
+        //打开外部浏览器
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.VIEW");
+        Uri content_url = Uri.parse("https://mobile.alipay.com/index.htm");
+        intent.setData(content_url);
+        activity.startActivity(intent);
     }
 
     public static void savePicToLocal(Activity context, ImageView iv_qr_code) {
