107e20e4fd792b9ba4e4230270edc8917235b382
L <unkown@.com>
2019-11-25 21:31:27 +0800

    优化

--- common/src/main/java/com/common/comm/input_check/CheckUtil.java
+++ common/src/main/java/com/common/comm/input_check/CheckUtil.java
@@ -1,23 +1,30 @@
 package com.common.comm.input_check;
 
-import android.widget.TextView;
+import android.content.Context;
 
-import com.common.utils.LogUtil;
+import com.common.utils.StringUtil;
+import com.common.utils.ToastUtil;
 
 public class CheckUtil {
 
-    public static boolean check(InputType[] types, TextView[] tvArr) {
-        if (types.length != tvArr.length) {
-            LogUtil.e("检测TextView数组和其对应类型 长度必须一致");
-            return false;
-        }
-        for (int i = 0; i < tvArr.length; i++) {
-            String inputText = tvArr[i].getText().toString();
-            boolean check = types[i].check(inputText);
-            if (!check) {
-                return false;
+    public static boolean checkAmount(String currentAmount, String min, String max, Context context) {
+        min = StringUtil.isFloat(min) ? min : "0";
+        max = StringUtil.isFloat(max) ? max : "1000000";
+        float minAmount = Float.parseFloat(min);
+        float maxAmount = Float.parseFloat(max);
+        if (StringUtil.isFloat(currentAmount)) {
+            float amount_f = Float.parseFloat(currentAmount);
+            if (amount_f < minAmount) {
+                ToastUtil.showShort("金额不能小于" + min + "元");
+            } else if (amount_f > maxAmount) {
+                ToastUtil.showShort("金额不能大于" + max + "元");
+            } else {
+                return true;
             }
+        } else {
+            ToastUtil.showShort("请输入合法金额");
         }
-        return true;
+        return false;
     }
+
 }
