7c5929cdf7aba4c772caa83a5c71ab7589dcc718
L <unkown@.com>
2020-01-16 19:49:24 +0800

    --

--- java_test/src/main/java/cglib/MyIntercept.java
+++ java_test/src/main/java/cglib/MyIntercept.java
@@ -1,18 +1,17 @@
 package cglib;
 
-import com.test.java.util.LogUtil;
 
-import net.sf.cglib.proxy.MethodInterceptor;
-import net.sf.cglib.proxy.MethodProxy;
+public class MyIntercept {
 
-import java.lang.reflect.Method;
+/*    public Object newInstall(Class<?> clazz) {
+        return Enhancer.create(clazz, this);
+    }
 
-public class MyIntercept implements MethodInterceptor {
     @Override
     public Object intercept(Object target, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
         LogUtil.d("函数执行前");
         Object rev = methodProxy.invoke(target, args);
         LogUtil.d("函数执行后");
         return rev;
-    }
+    }*/
 }
--- java_test/src/main/java/cglib/Start.java
+++ java_test/src/main/java/cglib/Start.java
@@ -4,11 +4,10 @@ package cglib;
 public class Start {
 
     public static void main(String[] args) {
-    //    FuncTest test = new FuncTest();
-    //    test.test1();
         startHook();
     }
 
     private static void startHook(){
+
     }
 }
