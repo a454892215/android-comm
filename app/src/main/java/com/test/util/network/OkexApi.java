f157062b82d0884542affea6017819d43794c94a
L <unkown@.com>
2020-01-30 14:23:05 +0800

    --

---
+++ app/src/main/java/com/test/util/network/OkexApi.java
@@ -0,0 +1,20 @@
+package com.test.util.network;
+
+import io.reactivex.Observable;
+import okhttp3.ResponseBody;
+import retrofit2.http.GET;
+
+public interface OkexApi {
+
+    String baseUrl = " https://data.gateio.co";
+    String commPath = "/api2/1/";
+
+    @GET(commPath + "pairs")
+    Observable<ResponseBody> getPairs();
+
+    @GET(commPath + "marketinfo")
+    Observable<ResponseBody> getMarketInfo();
+
+    @GET(commPath + "candlestick2/btc_usdt?group_set=600&range_hour=12")
+    Observable<ResponseBody> getCandleStick2();
+}
