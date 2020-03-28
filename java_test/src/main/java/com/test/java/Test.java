abb71efd0a09a54bc843a7b91303ea56bbfb5aa5
LiuPan <1573802540@qq.com>
2020-03-28 10:43:08 +0800

    test

--- java_test/src/main/java/com/test/java/Test.java
+++ java_test/src/main/java/com/test/java/Test.java
@@ -1,11 +1,28 @@
 package com.test.java;
 
+import com.test.java.util.LogUtil;
+
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Test {
 
     public static void main(String[] args) {
+        Timer timer = new Timer();
+        for (int i = 0; i < 5; i++) {
+         //   int random = new Random().nextInt(3) + 1;
+            int finalI = i;
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    LogUtil.d("===============random:" + finalI);
+                }
+            }, 1000 * i);
+
+        }
 
 
     }
