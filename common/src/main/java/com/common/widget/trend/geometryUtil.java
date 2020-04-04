8a59af06ea6b1a13f47f3d3f6a65b3bd6c931862
LiuPan <1573802540@qq.com>
2020-04-04 18:05:35 +0800

    获取两直线交点 工具类

--- common/src/main/java/com/common/widget/trend/geometryUtil.java
+++ common/src/main/java/com/common/widget/trend/geometryUtil.java
@@ -1,5 +1,11 @@
 package com.common.widget.trend;
 
+
+import com.common.utils.LogUtil;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Author:  L
  * CreateDate: 2019/1/29 9:27
@@ -38,5 +44,42 @@ class geometryUtil {
             return new Point[]{new Point(Math.round(x2), Math.round(y2)), new Point(Math.round(x1), Math.round(y1))};
         }
     }
+
+    /**
+     * 获取两直线交点坐标
+     * float x0, float y0, float x1, float y1 直线1
+     * float x2, float y2, float x3, float y3 直线2
+     *
+     * @return 交点
+     */
+    public static Point getIntersectionForTowLine(float x0, float y0, float x1, float y1, float x2, float y2, float x3, float y3) {
+/*       float a = y1 - y0;
+        float b = x1 * y0 - x0 * y1;
+        float c = x1 - x0;
+        float d = y3 - y2;
+        float e = x3 * y2 - x2 * y3;
+        float f = x3 - x2;
+        float y = (a * e - b * d) / (a * f - c * d);
+        float x = (y * c - b) / a;
+        return new Point(x, y);*/
+        try {
+            float y = ((y0 - y1) * (y3 - y2) * x0 + (y3 - y2) * (x1 - x0) * y0 + (y1 - y0) * (y3 - y2) * x2 + (x2 - x3) * (y1 - y0) * y2) / ((x1 - x0) * (y3 - y2) + (y0 - y1) * (x3 - x2));
+            float x = x2 + (x3 - x2) * (y - y2) / (y3 - y2);
+            ArrayList<Float> xList = new ArrayList<>();
+            xList.add(x0);
+            xList.add(x1);
+            xList.add(x2);
+            xList.add(x3);
+            Float minX = Collections.min(xList);
+            Float maxX = Collections.max(xList);
+            if (x >= minX && x <= maxX) {
+                return new Point(x, y);
+            }
+        } catch (Exception e) {
+            LogUtil.e(e);
+        }
+        return null;
+    }
+
 }
 
