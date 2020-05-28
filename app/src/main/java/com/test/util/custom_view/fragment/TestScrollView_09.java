b2054692b79c76898f2c120b39be68ee3a0c3db1
LiuPan <1573802540@qq.com>
2020-05-28 08:51:58 +0800

    自定义可滚动内容的View测试

---
+++ app/src/main/java/com/test/util/custom_view/fragment/TestScrollView_09.java
@@ -0,0 +1,18 @@
+package com.test.util.custom_view.fragment;
+
+import com.common.base.BaseFragment;
+import com.test.util.R;
+
+public class TestScrollView_09 extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_08;
+    }
+
+
+    @Override
+    protected void initView() {
+    }
+
+
+}
