5e5d4a2d06bbd6c8e0ca825ce45c9a85466fbbe6
LiuPan <1573802540@qq.com>
2020-05-29 08:41:44 +0800

    --

--- common/src/main/java/com/common/widget/entity/ViewItem.java
+++ common/src/main/java/com/common/widget/entity/ViewItem.java
@@ -16,18 +16,4 @@ public class ViewItem {
     public int index;
     public float offset;
 
-
-    /**
-     * 在View的左边超屏 不可见
-     */
-    public boolean isOnViewLeft(View view) {
-        return end < 0;
-    }
-
-    /**
-     * 在View的右边超屏 不可见
-     */
-    public boolean isOnViewRight(View view) {
-        return start > view.getMeasuredWidth();
-    }
 }
