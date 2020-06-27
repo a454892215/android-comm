170946335c387ef8742001649a35ace486a3e5e4
LiuPan <1573802540@qq.com>
2020-06-27 08:34:05 +0800

    --

---
+++ common/src/main/java/com/common/http/inter/HttpCallbackAdapter.java
@@ -0,0 +1,17 @@
+package com.common.http.inter;
+
+/**
+ * Author:  L
+ * CreateDate: 2019/1/18 9:21
+ * Description: No
+ */
+
+public class HttpCallbackAdapter implements HttpCallback  {
+    public void onSuccess(String text){
+
+    }
+
+    public void onFail(Throwable e){
+
+    }
+}
