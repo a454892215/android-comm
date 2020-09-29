43c2e11d8db9006cdf5eb6d301695c3defef2d84
LiuPan <1573802540@qq.com>
2020-09-30 07:34:48 +0800

    --

--- app/src/main/java/com/test/util/custom_view2/widget/TestCusView.java
+++ app/src/main/java/com/test/util/custom_view2/widget/TestCusView.java
@@ -3,12 +3,15 @@ package com.test.util.custom_view2.widget;
 import android.content.Context;
 import android.graphics.Canvas;
 
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 
 import android.view.View;
 
 import androidx.annotation.Nullable;
+
+import com.common.comm.L;
 
 
 /**
@@ -35,6 +38,7 @@ public class TestCusView extends View {
         super(context, attrs, defStyleAttr);
         this.context = context;
         paint = new Paint();
+        paint.setColor(Color.RED);
 
 
     }
@@ -42,6 +46,20 @@ public class TestCusView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        float startX = 0;
+        float startY = L.dp_1 * 20;
+        float endX = L.dp_1 * 280;
+        paint.setStrokeWidth(L.dp_1 * 10);
+        canvas.drawLine(startX, startY, endX, startY, paint);
+
+        // top = startY - StrokeWidth/2 = 20 -
+        canvas.drawLine(L.dp_1 * 300, startY, L.dp_1 * 300, startY + L.dp_1 * 10, paint);
+        paint.setStrokeWidth(1);
+        paint.setColor(Color.WHITE);
+        for (int i = 1; i < 200; i++) {
+            canvas.drawLine(0, i * 10 * L.dp_1, getWidth(), i * 10 * L.dp_1, paint);
+        }
+
 
     }
 
