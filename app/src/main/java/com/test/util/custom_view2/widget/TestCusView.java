96bebcc62a423b1eaeab5e39262f633911ab5b9d
LiuPan <1573802540@qq.com>
2020-09-30 07:56:58 +0800

    StrokeCap示例

--- app/src/main/java/com/test/util/custom_view2/widget/TestCusView.java
+++ app/src/main/java/com/test/util/custom_view2/widget/TestCusView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 
 import android.view.View;
@@ -22,6 +23,7 @@ import com.common.comm.L;
 
 public class TestCusView extends View {
     private Paint paint;
+    private Paint textPaint;
     private Context context;
 
 
@@ -40,27 +42,53 @@ public class TestCusView extends View {
         paint = new Paint();
         paint.setColor(Color.RED);
 
+        textPaint = new TextPaint();
+        textPaint.setTextSize(L.dp_1 * 12);
+        textPaint.setColor(Color.WHITE);
+        textPaint.setTextAlign(Paint.Align.CENTER);
+
 
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        float startX = 0;
-        float startY = L.dp_1 * 20;
-        float endX = L.dp_1 * 280;
-        paint.setStrokeWidth(L.dp_1 * 10);
-        canvas.drawLine(startX, startY, endX, startY, paint);
+        testStrokeCap(canvas);
 
-        // top = startY - StrokeWidth/2 = 20 -
-        canvas.drawLine(L.dp_1 * 300, startY, L.dp_1 * 300, startY + L.dp_1 * 10, paint);
         paint.setStrokeWidth(1);
         paint.setColor(Color.WHITE);
-        for (int i = 1; i < 200; i++) {
+        for (int i = 1; i < 20; i++) {
             canvas.drawLine(0, i * 10 * L.dp_1, getWidth(), i * 10 * L.dp_1, paint);
         }
 
 
+    }
+
+    private void testStrokeCap(Canvas canvas) {
+        float startX = L.dp_1 * 5;
+        float startY = L.dp_1 * 10;
+        float endX = L.dp_1 * 280;
+        paint.setStrokeWidth(L.dp_1 * 10);
+        paint.setStrokeCap(Paint.Cap.BUTT);
+        canvas.drawLine(startX, L.dp_1 * 20, endX, L.dp_1 * 20, paint);
+
+        paint.setStrokeCap(Paint.Cap.ROUND); // 加入StrokeWidth为半径的原型头尾
+        canvas.drawLine(startX, L.dp_1 * 40, endX, L.dp_1 * 40, paint);
+
+        paint.setStrokeCap(Paint.Cap.SQUARE);// 加入StrokeWidth为长度的矩形头尾
+        canvas.drawLine(startX, L.dp_1 * 60, endX, L.dp_1 * 60, paint);
+
+        // top = startY - StrokeWidth/2 = 20 -
+        paint.setStrokeCap(Paint.Cap.BUTT);
+        canvas.drawLine(L.dp_1 * 300, startY, L.dp_1 * 300, startY + L.dp_1 * 50, paint);
+
+        paint.setStrokeCap(Paint.Cap.ROUND); // 加入StrokeWidth为半径的原型头尾
+        canvas.drawLine(L.dp_1 * 320, startY, L.dp_1 * 320, startY + L.dp_1 * 50, paint);
+
+        paint.setStrokeCap(Paint.Cap.SQUARE);// 加入StrokeWidth为长度的矩形头尾
+        canvas.drawLine(L.dp_1 * 340, startY, L.dp_1 * 340, startY + L.dp_1 * 50, paint);
+
+        canvas.drawText("StrokeCap 三种类型示例", getWidth() / 2f, L.dp_1 * 80, textPaint);
     }
 
 }
