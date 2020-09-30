1e368af4426c372f943c0050ee95b60a1b5fa063
LiuPan <1573802540@qq.com>
2020-09-30 20:26:46 +0800

    新示例

--- app/src/main/java/com/test/util/custom_view2/Fragment_02.java
+++ app/src/main/java/com/test/util/custom_view2/Fragment_02.java
@@ -7,7 +7,7 @@ import com.test.util.R;
 public class Fragment_02 extends BaseFragment {
     @Override
     protected int getLayoutId() {
-        return R.layout.new_cus_viewfrag_01;
+        return R.layout.new_cus_viewfrag_02;
     }
 
     @Override
