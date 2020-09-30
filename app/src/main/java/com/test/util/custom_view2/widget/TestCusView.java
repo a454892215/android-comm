edfb919a62c4718df5e9fdc98b5ee9623edb9de3
LiuPan <1573802540@qq.com>
2020-09-30 18:02:54 +0800

    图层混合模式 示例

--- app/src/main/java/com/test/util/custom_view2/widget/TestCusView.java
+++ app/src/main/java/com/test/util/custom_view2/widget/TestCusView.java
@@ -5,6 +5,8 @@ import android.graphics.Canvas;
 
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 
@@ -13,6 +15,7 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 import com.common.comm.L;
+import com.common.utils.ViewUtil;
 
 
 /**
@@ -25,6 +28,7 @@ public class TestCusView extends View {
     private Paint paint;
     private Paint textPaint;
     private Context context;
+    private PorterDuffXfermode xfermode;
 
 
     public TestCusView(Context context) {
@@ -39,31 +43,47 @@ public class TestCusView extends View {
     public TestCusView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         this.context = context;
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null); // 图层混合模式使用 必须关闭硬件加速
         paint = new Paint();
-
         textPaint = new TextPaint();
         textPaint.setTextSize(L.dp_1 * 12);
         textPaint.setColor(Color.WHITE);
         textPaint.setTextAlign(Paint.Align.CENTER);
 
-
+        xfermode = new PorterDuffXfermode(PorterDuff.Mode.DST_OUT);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        testStrokeCap(canvas);
-
-        paint.setStrokeWidth(1);
-        paint.setColor(Color.WHITE);
-        for (int i = 1; i < 20; i++) {
-            canvas.drawLine(0, i * 10 * L.dp_1, getWidth(), i * 10 * L.dp_1, paint);
-        }
-
-
+        float bottom = testStrokeCap(canvas);
+        testXMode(canvas, bottom);
+        drawGrid(canvas);
     }
 
-    private void testStrokeCap(Canvas canvas) {
+    private void testXMode(Canvas canvas, float bottom) {
+        paint.setXfermode(null);
+        paint.setStrokeWidth(1);
+        paint.setColor(Color.parseColor("#58985A"));
+        float radius = L.dp_1 * 30;
+        float x = radius + L.dp_1 * 10;
+        float y = bottom + radius + L.dp_1 * 10;
+        canvas.drawCircle(x, y, radius, paint);
+        paint.setColor(Color.YELLOW);
+        paint.setXfermode(xfermode);
+        canvas.drawCircle(x + radius, y, radius, paint);
+        paint.setXfermode(null);
+    }
+
+    private void drawGrid(Canvas canvas) {
+        paint.setStrokeWidth(1);
+        paint.setColor(Color.WHITE);
+        for (int i = 1; i < 60; i++) {
+            canvas.drawLine(0, i * 10 * L.dp_1, getWidth(), i * 10 * L.dp_1, paint);
+        }
+    }
+
+    private float testStrokeCap(Canvas canvas) {
         float startX = L.dp_1 * 5;
         float startY = L.dp_1 * 10;
         float endX = L.dp_1 * 280;
@@ -88,7 +108,9 @@ public class TestCusView extends View {
         paint.setStrokeCap(Paint.Cap.SQUARE);// 加入StrokeWidth为长度的矩形头尾
         canvas.drawLine(L.dp_1 * 340, startY, L.dp_1 * 340, startY + L.dp_1 * 50, paint);
 
-        canvas.drawText("StrokeCap 三种类型示例", getWidth() / 2f, L.dp_1 * 80, textPaint);
+        float bottomTextCenter = L.dp_1 * 80;
+        canvas.drawText("StrokeCap 三种类型示例", getWidth() / 2f, ViewUtil.getBaseLine(textPaint, bottomTextCenter), textPaint);
+        return bottomTextCenter + L.dp_1 * 7;
     }
 
 }
