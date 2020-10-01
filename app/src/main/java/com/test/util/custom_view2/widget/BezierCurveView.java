4de0bfdfecb083186dd1de3a993d5962d8d64ef6
LiuPan <1573802540@qq.com>
2020-10-01 21:02:17 +0800

    --

--- app/src/main/java/com/test/util/custom_view2/widget/BezierCurveView.java
+++ app/src/main/java/com/test/util/custom_view2/widget/BezierCurveView.java
@@ -37,7 +37,6 @@ public class BezierCurveView extends View {
         super(context, attrs, defStyleAttr);
         setLayerType(View.LAYER_TYPE_SOFTWARE, null); // 图层混合模式使用 必须关闭硬件加速
         paint = new Paint();
-        paint.setColor(Color.GREEN);
         path = new Path();
 
     }
@@ -46,13 +45,27 @@ public class BezierCurveView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        path.moveTo(L.dp_1 * 20, L.dp_1 * 100); // 起点
+        paint.setStyle(Paint.Style.STROKE);
+        float startX = L.dp_1 * 20;
+        float StartY = L.dp_1 * 100;
+
         float controlX = L.dp_1 * 60;
         float controlY = L.dp_1 * 100;
-        path.quadTo(controlX, controlY, L.dp_1 * 220, L.dp_1 * 100); // 控制点和终点
-        canvas.drawPath(path, paint);
 
+        float endX = L.dp_1 * 220;
+        float endY = L.dp_1 * 100;
+
+        path.moveTo(startX, StartY); // 起点
+        path.quadTo(controlX, controlY, endX, endY); // 控制点和终点
+        paint.setColor(Color.GREEN);
+        canvas.drawPath(path, paint);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.RED);
+        canvas.drawCircle(startX, StartY, L.dp_1 * 3, paint); // 绘制起点
+        paint.setColor(Color.BLUE);
         canvas.drawCircle(controlX, controlY, L.dp_1 * 3, paint); // 绘制控制点
+        paint.setColor(Color.YELLOW);
+        canvas.drawCircle(endX, endY, L.dp_1 * 3, paint); // 绘制控终点
 
     }
 
