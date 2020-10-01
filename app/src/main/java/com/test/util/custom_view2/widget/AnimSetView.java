df9229938a1e2f10dacd53e06d5eaa6df63ddc8c
LiuPan <1573802540@qq.com>
2020-10-01 18:29:20 +0800

    背景

--- app/src/main/java/com/test/util/custom_view2/widget/AnimSetView.java
+++ app/src/main/java/com/test/util/custom_view2/widget/AnimSetView.java
@@ -1,18 +1,29 @@
 package com.test.util.custom_view2.widget;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.LinearInterpolator;
 
 import androidx.annotation.Nullable;
 
 import com.common.comm.L;
-import com.common.utils.MathUtil;
+import com.common.utils.ViewUtil;
+import com.test.util.R;
+
+import org.jetbrains.annotations.NotNull;
 
 
 /**
@@ -23,21 +34,19 @@ import com.common.utils.MathUtil;
 
 public class AnimSetView extends View {
     private Paint mPaint = new Paint();
-    private Paint mHolePaint = new Paint();
-    private int mbgColor = Color.WHITE;
     private int[] color_arr = {Color.RED, Color.YELLOW, Color.BLACK, Color.BLUE, Color.GREEN, Color.CYAN};
     // 旋转圆中心坐标
     private float centerX;
     private float centerY;
 
     // 最大扩散圆半径
-    private float circle_radius = L.dp_1 * 16; //旋转圆半径
-    private float min_circle_radius = L.dp_1 * 3; //旋转圆半径
-    private float max_circle_radius = L.dp_1 * 24;
+    private float cur_circle_radius = L.dp_1 * 20; //旋转圆半径
+    private float min_circle_radius = L.dp_1 * 3;
+    private float max_circle_radius = L.dp_1 * 28;
     // 6个小圆半径
     private float ball_radius = L.dp_1 * 3;
     private float curAngle = 0;
-    private ValueAnimator animator_1;
+    private BitmapDrawable bitmapDrawable;
 
 
     public AnimSetView(Context context) {
@@ -48,53 +57,56 @@ public class AnimSetView extends View {
         this(context, attrs, -1);
     }
 
+    private static final float rotate_v = 4f; // 圆环旋转速度
 
     public AnimSetView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         setLayerType(View.LAYER_TYPE_SOFTWARE, null); // 图层混合模式使用 必须关闭硬件加速
-        mHolePaint.setColor(mbgColor);
-
-        animator_1 = ValueAnimator.ofFloat(0, 1);
-        animator_1.setRepeatCount(-1);
-        animator_1.setDuration(2000);
-        animator_1.setInterpolator(new LinearInterpolator());
-        animator_1.addUpdateListener(animation -> {
-            updateUI();
-            invalidate();
-        });
+        ValueAnimator animator_1 = prepareAnim1();
         setOnClickListener(v -> {
-                    to_max_radius_count = 0;
-                    animator_1.start();
-                }
-        );
+            cur_circle_radius = L.dp_1 * 20; //旋转圆半径
+            animator_1.start();
+        });
 
         setOnLongClickListener(v -> {
             animator_1.pause();
             animator_1.cancel();
-            to_max_radius_count = 0;
             invalidate();
             return true;
         });
-
+        Bitmap bitmap_fj = BitmapFactory.decodeResource(context.getResources(), R.mipmap.feng_jing_pic);
+        bitmapDrawable = new BitmapDrawable(context.getResources(), bitmap_fj);
     }
 
-    private float rotate_radius_v = 0.5f; // 旋转半径变化速度
-    private static final float rotate_v = 4f; // 圆环旋转速度
-    private int to_max_radius_count = 0;
-
-    private void updateUI() {
-        curAngle += Math.PI / 180f * rotate_v;
-        if (circle_radius == min_circle_radius) rotate_radius_v *= -1;
-        if (circle_radius == max_circle_radius) {
-            rotate_radius_v *= -1;
-            to_max_radius_count++;
-            if (animator_1.isRunning() && to_max_radius_count == 2) {
-                animator_1.pause();
-                animator_1.cancel();
+    @NotNull
+    private ValueAnimator prepareAnim1() {
+        ValueAnimator animator_1 = ValueAnimator.ofFloat(0, 1);
+        animator_1.setDuration(1000);
+        animator_1.setInterpolator(new LinearInterpolator());
+        animator_1.addUpdateListener(animation -> {
+            float change_angle = (float) (Math.PI / 180f * rotate_v);
+            curAngle += change_angle;
+            invalidate();
+        });
+        animator_1.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                startAnim2();
             }
-        }
-        circle_radius += rotate_radius_v;
-        circle_radius = MathUtil.clamp(circle_radius, min_circle_radius, max_circle_radius);
+        });
+        return animator_1;
+    }
+
+    private void startAnim2() {
+        ValueAnimator animator_2 = ValueAnimator.ofFloat(cur_circle_radius, max_circle_radius, min_circle_radius);
+        animator_2.setDuration(800);
+        animator_2.setInterpolator(new AccelerateInterpolator());
+        animator_2.addUpdateListener(anim -> {
+            cur_circle_radius = (float) anim.getAnimatedValue();
+            invalidate();
+        });
+        animator_2.start();
     }
 
     @Override
@@ -102,17 +114,20 @@ public class AnimSetView extends View {
         super.onSizeChanged(w, h, oldw, oldh);
         centerX = getMeasuredWidth() / 2f;
         centerY = getMeasuredHeight() / 2f;
+        int pic_bottom = (int) ViewUtil.getPicHeightByWidth(bitmapDrawable, getMeasuredWidth());
+        bitmapDrawable.setBounds(0, 0, getMeasuredWidth(), pic_bottom);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        bitmapDrawable.draw(canvas);
         float per_rad = (float) (Math.PI * 2 / color_arr.length);
         // 绘制6个小球
         for (int i = 0; i < color_arr.length; i++) {
             float angle = per_rad * i + curAngle;
-            float cx = (float) (centerX + circle_radius * Math.cos(angle));
-            float cy = (float) (centerY + circle_radius * Math.sin(angle));
+            float cx = (float) (centerX + cur_circle_radius * Math.cos(angle));
+            float cy = (float) (centerY + cur_circle_radius * Math.sin(angle));
             mPaint.setColor(color_arr[i]);
             canvas.drawCircle(cx, cy, ball_radius, mPaint);
         }
