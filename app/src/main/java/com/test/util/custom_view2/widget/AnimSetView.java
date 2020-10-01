cd5e3dcfa2c80aca85892a5063711f737e829adc
LiuPan <1573802540@qq.com>
2020-10-01 12:49:32 +0800

    --

--- app/src/main/java/com/test/util/custom_view2/widget/AnimSetView.java
+++ app/src/main/java/com/test/util/custom_view2/widget/AnimSetView.java
@@ -1,13 +1,18 @@
 package com.test.util.custom_view2.widget;
 
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
+
 import androidx.annotation.Nullable;
 
+import com.common.comm.L;
+import com.common.utils.LogUtil;
 
 
 /**
@@ -17,9 +22,20 @@ import androidx.annotation.Nullable;
  */
 
 public class AnimSetView extends View {
-    private Paint paint;
-    private Paint textPaint;
-    private Matrix matrix;
+    private Paint mPaint = new Paint();
+    private Paint mHolePaint = new Paint();
+    private ValueAnimator valueAnimator;
+    private int mbgColor = Color.WHITE;
+    private int[] color_arr = {Color.RED, Color.YELLOW, Color.BLACK, Color.BLUE, Color.GREEN, Color.CYAN};
+    // 旋转圆中心坐标
+    private float centerX;
+    private float centerY;
+
+    // 最大扩散圆半径
+    private float max_radius = L.dp_1 * 30;
+    private float circle_radius = L.dp_1 * 20; //旋转圆半径
+    // 6个小圆半径
+    private float ball_radius = L.dp_1 * 3;
 
 
     public AnimSetView(Context context) {
@@ -34,15 +50,31 @@ public class AnimSetView extends View {
     public AnimSetView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         setLayerType(View.LAYER_TYPE_SOFTWARE, null); // 图层混合模式使用 必须关闭硬件加速
-        paint = new Paint();
+        mHolePaint.setColor(mbgColor);
 
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        centerX = getMeasuredWidth() / 2f;
+        centerY = getMeasuredHeight() / 2f;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        float per_rad = (float) (Math.PI * 2 / color_arr.length);
+        // 绘制6个小球
+        for (int i = 0; i < color_arr.length; i++) {
+            float angle = per_rad * i;
+            float cx = (float) (centerX + circle_radius * Math.cos(angle));
+            float cy = (float) (centerY + circle_radius * Math.sin(angle));
+            LogUtil.d("cx:" + cx + " cy:" + cy + "  index:" + i + "  angle:" + angle);
+            mPaint.setColor(color_arr[i]);
+            canvas.drawCircle(cx, cy, ball_radius, mPaint);
+        }
     }
-
 
 
 }
