fa0591fbbbc258ffd723701b43c2d0be9951aca0
LiuPan <1573802540@qq.com>
2020-10-01 21:51:49 +0800

    --

--- app/src/main/java/com/test/util/custom_view2/widget/BezierCurveView.java
+++ app/src/main/java/com/test/util/custom_view2/widget/BezierCurveView.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -22,7 +23,8 @@ import com.common.comm.L;
 public class BezierCurveView extends View {
     private Paint paint;
     private Path path;
-
+    private float controlX = L.dp_1 * 90;
+    private float controlY = L.dp_1 * 150;
 
     public BezierCurveView(Context context) {
         this(context, null);
@@ -38,26 +40,30 @@ public class BezierCurveView extends View {
         setLayerType(View.LAYER_TYPE_SOFTWARE, null); // 图层混合模式使用 必须关闭硬件加速
         paint = new Paint();
         path = new Path();
-
     }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        controlX = event.getX();
+        controlY = event.getY();
+        invalidate();
+        return true;
+    }
+
 
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        float startX = L.dp_1 * 20;
+        path.reset(); // 必须清空
+        float startX = L.dp_1 * 80;
         float StartY = L.dp_1 * 200;
-
-        float controlX = L.dp_1 * 90;
-        float controlY = L.dp_1 * 150;
-
-        float endX = L.dp_1 * 220;
+        float endX = L.dp_1 * 280;
         float endY = L.dp_1 * 200;
 
         path.moveTo(startX, StartY); // 起点
         path.quadTo(controlX, controlY, endX, endY); // 控制点和终点
-        paint.setColor(Color.BLACK);
+        paint.setColor(Color.CYAN);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(L.dp_1 * 3);
         canvas.drawPath(path, paint);
