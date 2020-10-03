5fbd35683041d3c0c32ce2877860917fb29a48ca
LiuPan <1573802540@qq.com>
2020-10-03 10:12:17 +0800

    新增接口

---
+++ common/src/main/java/com/common/listener/OnFinishedListener.java
@@ -0,0 +1,12 @@
+package com.common.listener;
+
+/**
+ * Author:  Pan
+ * CreateDate: 2019/10/23 9:08
+ * Description: No
+ */
+
+public interface OnFinishedListener {
+
+    void onFinished(Object object);
+}
