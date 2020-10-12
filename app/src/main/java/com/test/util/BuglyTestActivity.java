cebeb40025796c716b82d397fbb4477039ca6f5c
LiuPan <1573802540@qq.com>
2020-10-12 21:45:22 +0800

    异常验证

--- app/src/main/java/com/test/util/BuglyTestActivity.java
+++ app/src/main/java/com/test/util/BuglyTestActivity.java
@@ -1,24 +1,43 @@
 package com.test.util;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.tencent.bugly.crashreport.CrashReport;
 import com.test.util.base.MyBaseActivity;
+
+import java.util.concurrent.Executors;
 
 public class BuglyTestActivity extends MyBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        findViewById(R.id.btn_1).setOnClickListener(v -> CrashReport.testJavaCrash());
-        findViewById(R.id.btn_2).setOnClickListener(v -> {
-            int a = 0;
-            int b = 0;
-            int c = a/b;
-        });
-        findViewById(R.id.btn_3).setOnClickListener(v -> {
-            BuglyTestActivity activity = null;
-            activity.getLayoutId();
-        });
+        findViewById(R.id.btn_java_main_thread_e_1).setOnClickListener(this);
+        findViewById(R.id.btn_java_io_thread_e_1).setOnClickListener(this);
+        findViewById(R.id.btn_main_to_c_e).setOnClickListener(this);
+        findViewById(R.id.btn_io_to_c_e).setOnClickListener(this);
+        findViewById(R.id.btn_bugly_test).setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.btn_java_main_thread_e_1:
+                throw new RuntimeException("java层 主线程 自定义RuntimeException异常");
+            case R.id.btn_java_io_thread_e_1:
+                Executors.newSingleThreadExecutor().execute(() -> {
+                    throw new RuntimeException("java层 子线程 自定义RuntimeException异常");
+                });
+                break;
+            case R.id.btn_main_to_c_e:
+                throw new RuntimeException("java主线程  到C代码 异常");
+            case R.id.btn_io_to_c_e:
+                throw new RuntimeException("java子线程  到C代码 异常");
+            case R.id.btn_bugly_test:
+                CrashReport.testJavaCrash();
+        }
     }
 
     public String getText() {
@@ -27,6 +46,6 @@ public class BuglyTestActivity extends MyBaseActivity {
 
     @Override
     protected int getLayoutId() {
-        return R.layout.activity_progress_live_test;
+        return R.layout.activity_exception_test;
     }
 }
