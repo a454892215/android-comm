9aa0cf2c586365f3e9a24e7277f6c78bdea6f55b
LiuPan <1573802540@qq.com>
2020-10-23 22:15:50 +0800

    --

--- app/src/main/java/com/kotl/Test.kt
+++ app/src/main/java/com/kotl/Test.kt
@@ -1,10 +1,28 @@
 package com.kotl
 
+import java.util.*
+
 /**
  * Author: Pan
  * 2020/10/17
  *  Description:
  */
-fun main() {
-    print("你哈")
+
+class Mime {
+
+    // 即isEmpty这个属性，是判断该类的size属性是否等于0
+    var name: String = ""
+        get() = "随意怎么修改都不会改变"
+
+
+    var str2 = ""
 }
+
+// 测试
+fun main(args: Array<String>) {
+    val mime = Mime()
+    println("name = ${mime.name}")
+    mime.str2 = "你好"
+    println("str2 = ${mime.str2}")
+}
+
