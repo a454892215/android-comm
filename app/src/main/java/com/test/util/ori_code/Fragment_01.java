d6447cd1c2a3ec56b5162abbc2981a7926415c58
LiuPan <1573802540@qq.com>
2020-10-24 11:22:14 +0800

    --

--- app/src/main/java/com/test/util/ori_code/Fragment_01.java
+++ app/src/main/java/com/test/util/ori_code/Fragment_01.java
@@ -3,7 +3,6 @@ package com.test.util.ori_code;
 import android.annotation.SuppressLint;
 import android.os.Handler;
 import android.os.Message;
-import android.view.View;
 import android.widget.TextView;
 
 import com.common.base.BaseFragment;
@@ -20,12 +19,31 @@ public class Fragment_01 extends BaseFragment {
         return R.layout.ori_code_fragment_1;
     }
 
-    //写法1： handler1 handler2 的区别是什么？
-    Handler handler1 = new Handler(new Handler.Callback() {
+
+    /**
+     * /写法1： 匿名内部类写法 （编译器有内存泄露警告）
+     * 此时即使把 handler1 设为static也依然会有内存泄露警告（问题？ 既然设置为static 并且编译通过，那么匿名内部类对象Handler1是不会依赖外部对象的，
+     * 因为静态成员初始化优先于对象，为什么还会有内存泄露警告？）
+     */
+    private Handler handler1 = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == 1) {
+                textInfo.setText("handler2收到了信息： what:" + msg.what);
+            }
+        }
+    };
+
+
+    /**
+     * 写法2：（编译器无内存泄露警告） handler1 handler2 的区别是什么？
+     */
+    Handler handler2 = new Handler(new Handler.Callback() {
         @SuppressLint("SetTextI18n")
         @Override
         public boolean handleMessage(Message msg) {
-            if (msg.what == 1) {
+            if (msg.what == 3) {
                 textInfo.setText("handler1收到了信息： what:" + msg.what);
             }
             return false;
@@ -33,22 +51,8 @@ public class Fragment_01 extends BaseFragment {
     });
 
     /**
-     * /写法2： 匿名内部类写法 编译器有内存泄露警告
-     * 此时即使把 handler2 设为static也依然会有内存泄露警告（问题？ 既然设置为static 并且编译通过，那么Handler是不会依赖外部对象的，
-     * 因为静态成员初始化优先于对象，为什么还会有内存泄露警告？）
+     * 写法3：静态内部类写法（编译器无警告，静态内部类不依赖外部类的对象）
      */
-    private Handler handler2 = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            if (msg.what == 3) {
-                textInfo.setText("handler2收到了信息： what:" + msg.what);
-            }
-        }
-    };
-
-
-    //写法3：静态内部类写法（编译器无警告，静态内部类不依赖外部类的对象）
     private static class MyHandle extends Handler {
         WeakReference<Fragment_01> weakReference;
 
@@ -74,17 +78,11 @@ public class Fragment_01 extends BaseFragment {
     protected void initView() {
 
         textInfo = findViewById(R.id.text_info);
-        findViewById(R.id.btn_1).setOnClickListener(v -> {
-            handler1.sendEmptyMessage(1);
-        });
+        findViewById(R.id.btn_1).setOnClickListener(v -> handler1.sendEmptyMessage(1));
 
-        findViewById(R.id.btn_2).setOnClickListener(v -> {
-            handler2.sendEmptyMessage(3);
-        });
+        findViewById(R.id.btn_2).setOnClickListener(v -> handler2.sendEmptyMessage(3));
 
-        findViewById(R.id.btn_3).setOnClickListener(v -> {
-            new MyHandle(this).sendEmptyMessage(6);
-        });
+        findViewById(R.id.btn_3).setOnClickListener(v -> new MyHandle(this).sendEmptyMessage(6));
     }
 
 }
