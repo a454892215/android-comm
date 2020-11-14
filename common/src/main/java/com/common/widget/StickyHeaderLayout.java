8891e5e619e0b9472c28e38c48c6c77216b7a371
LiuPan <1573802540@qq.com>
2020-11-14 22:27:23 +0800

    -

---
+++ common/src/main/java/com/common/widget/StickyHeaderLayout.java
@@ -0,0 +1,238 @@
+package com.common.widget;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.LinearLayout;
+import android.widget.Scroller;
+
+import androidx.annotation.Nullable;
+
+import com.common.R;
+import com.common.comm.L;
+import com.common.widget.entity.ViewItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author: L
+ * 2020/11/24
+ * Description:
+ */
+@SuppressWarnings("unused")
+public class StickyHeaderLayout extends LinearLayout {
+
+    private VelocityTracker velocityTracker;
+    private ValueAnimator anim;
+    private float min_scroll_unit;
+    protected Scroller mScroller;
+    private int maxVelocity;
+    private ValueAnimator flingAnim;
+
+    public StickyHeaderLayout(Context context) {
+        this(context, null);
+    }
+
+    public StickyHeaderLayout(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, -1);
+    }
+
+
+    public StickyHeaderLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        min_scroll_unit = getResources().getDimension(R.dimen.dp_2);
+        mScroller = new Scroller(context);
+        mScroller.forceFinished(true);
+        maxVelocity = (int) L.dp_1 * 1500;
+        if (velocityTracker == null) {
+            velocityTracker = VelocityTracker.obtain();
+        }
+    }
+
+    private List<ViewItem> testTotalData = new ArrayList<>();
+
+    float startX;
+    float startY;
+    int orientation = 0;
+    int orientation_vertical = 1;
+    int orientation_horizontal = 2;
+
+    private static final int max_compute_times = 3;
+    private int compute_times = 0;
+    private float xScrollSum;
+    private float yScrollSum;
+    private float dx;
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        onInterceptTouchEvent(ev);
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            velocityTracker.clear();
+        }
+        velocityTracker.addMovement(ev);
+        return true;
+    }
+
+    private void preHandleMotionEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (flingAnim != null) flingAnim.cancel();
+                startX = ev.getRawX();
+                startY = ev.getRawY();
+                orientation = 0;
+                xScrollSum = 0;
+                yScrollSum = 0;
+                compute_times = 0;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float currentX = ev.getRawX();
+                float currentY = ev.getRawY();
+                dx = currentX - startX;
+                float dy = currentY - startY;
+                startX = currentX;
+                startY = currentY;
+
+                if (compute_times < max_compute_times || (Math.abs(xScrollSum) < min_scroll_unit) && Math.abs(xScrollSum) < min_scroll_unit) {
+                    xScrollSum += dx;
+                    yScrollSum += dy;
+                    compute_times++;
+                } else {
+                    if (Math.abs(xScrollSum) > Math.abs(yScrollSum)) {
+                        if (orientation == 0) orientation = orientation_horizontal;
+                    } else if (Math.abs(yScrollSum) > Math.abs(xScrollSum)) {
+                        if (orientation == 0) orientation = orientation_vertical;
+                    }
+                }
+                break;
+        }
+        if (orientation == orientation_horizontal) {
+            onTouchEvent(ev);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        super.onTouchEvent(ev);
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+                executeScrollXBy(-Math.round(dx));
+                break;
+            case MotionEvent.ACTION_UP:
+                performClick();
+                if (Math.abs(dx) > min_scroll_unit / 2) {
+                    velocityTracker.computeCurrentVelocity(2000, maxVelocity);
+                    float xVelocity = velocityTracker.getXVelocity();
+                    mScroller.abortAnimation();
+                    //  mScroller.fling(mScroller.getFinalX(), 0, -Math.round(xVelocity), 0, 0, (int) maxScrollWidth, 0, 0);
+                    if (flingAnim != null) flingAnim.cancel();
+                    flingAnim = ValueAnimator.ofFloat(getFlingDistance(xVelocity), 0);
+                    flingAnim.setDuration(Math.abs((int) xVelocity) / 2);
+                    flingAnim.setInterpolator(new DecelerateInterpolator());
+                    flingAnim.addUpdateListener(animation -> {
+                        float value = (float) animation.getAnimatedValue();
+                        executeScrollXBy(-value);
+                    });
+                    flingAnim.start();
+                    invalidate();
+                }
+                return false;
+        }
+        return true;
+    }
+
+    public float getMaxScrollWidth() {
+        return maxScrollWidth;
+    }
+
+    protected float maxScrollWidth;
+
+    protected float getFlingDistance(float xVelocity) {
+        return xVelocity / 100 * L.dp_1;
+    }
+
+
+    @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
+
+    @Override
+    public void computeScroll() {
+        if (mScroller.computeScrollOffset()) {
+            //  scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+            if(onScrollListener != null){
+                onScrollListener.onScroll(mScroller.getFinalX());
+            }
+            invalidate();
+        }
+        super.computeScroll();
+    }
+
+    protected boolean scrollEnable = true;
+
+    public void setScrollEnable(boolean scrollEnable) {
+        this.scrollEnable = scrollEnable;
+    }
+
+    public boolean isScrollEnable() {
+        return scrollEnable;
+    }
+
+
+    private void executeScrollXBy(float dx) {
+        if (!scrollEnable) return;
+        if (mScroller.getFinalX() + dx < 0) {//getFinalX 避免延迟
+            dx = 0f - mScroller.getFinalX();
+        }
+        if (mScroller.getFinalX() + dx > maxScrollWidth) {
+            dx = maxScrollWidth - mScroller.getFinalX();
+        }
+        mScroller.startScroll(mScroller.getFinalX(), 0, (int) dx, 0, 180);
+        invalidate();
+    }
+
+    public void executeScrollToX(float x) {
+        if (!scrollEnable) return;
+        if (x < 0) x = 0;
+        if (x > maxScrollWidth) x = maxScrollWidth;
+        dx = x - mScroller.getFinalX();
+        mScroller.startScroll(mScroller.getFinalX(), 0, (int) dx, 0, 180);
+        invalidate();
+    }
+
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (anim != null) {
+            anim.cancel();
+            anim = null;
+        }
+        if (velocityTracker != null) {
+            velocityTracker.recycle();
+            velocityTracker = null;
+        }
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if (velocityTracker == null) {
+            velocityTracker = VelocityTracker.obtain();
+        }
+    }
+
+    public void setOnScrollListener(OnScrollListener onScrollListener) {
+        this.onScrollListener = onScrollListener;
+    }
+
+    private OnScrollListener onScrollListener;
+
+    public interface OnScrollListener{
+       void onScroll(float currentX);
+    }
+}
