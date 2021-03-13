04856486b1a3bfc17e32f8bf6e78d949b6bb15d4
LiuPan <1573802540@qq.com>
2021-03-14 00:37:40 +0800

    --

--- java_test/src/main/java/adbforword/AndroidLogCat.java
+++ java_test/src/main/java/adbforword/AndroidLogCat.java
@@ -3,11 +3,8 @@ package adbforword;
 import com.test.java.util.LogUtil;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 
 /**
  * Author: Pan
