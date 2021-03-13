5433405f6d4ab91358c83f5ddc4387eb300c9a90
LiuPan <1573802540@qq.com>
2021-03-14 00:37:01 +0800

    --

--- java_test/src/main/java/adbforword/AndroidLogCat.java
+++ java_test/src/main/java/adbforword/AndroidLogCat.java
@@ -1,5 +1,14 @@
 package adbforword;
 
+import com.test.java.util.LogUtil;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
 /**
  * Author: Pan
  * 2021/3/13
@@ -7,7 +16,24 @@ package adbforword;
  */
 class AndroidLogCat {
 
-    public static void main(String[] args) {
+    private static void exeCmd() {
+        try {
+            String ins_1 = "cmd.exe /c dir " + System.getProperty("user.home") + "&&";
+            String ins_2 = "adb devices && adb logcat -c && adb logcat -g && adb logcat | find \"LLpp\"";
+            Process process = Runtime.getRuntime().exec(ins_1 + ins_2, null, null);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream(), Charset.forName("GBK")));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                LogUtil.d(line);
+            }
+            bufferedReader.close();
+            LogUtil.d("执行结果：" + process.waitFor());
+        } catch (Exception e) {
+            LogUtil.e("======:" + e);
+        }
+    }
 
+    public static void main(String[] args) {
+        exeCmd();
     }
 }
