5dff0f43b546cde5d50bdc8f27549202664c1510
LiuPan <1573802540@qq.com>
2021-04-04 01:16:55 +0800

    --

--- buildSrc/src/main/groovy/com/plugin/PluginImpl.groovy
+++ buildSrc/src/main/groovy/com/plugin/PluginImpl.groovy
@@ -6,8 +6,7 @@ import org.gradle.api.Project
 class PluginImpl2 implements Plugin<Project>{
 
     void apply(Project project){
-        System.out.println("========================");
-        System.out.println("hello gradle plugin!");
-        System.out.println("========================");
+        System.out.println("==========PluginImpl2==============");
+
     }
 }
