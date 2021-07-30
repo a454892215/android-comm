ffe420e40193a0da38926aa45087c25a60b23165
LiuPan <1573802540@qq.com>
2021-07-30 23:55:59 +0800

    测试示例

--- app/src/main/java/com/database/LitePalTest.java
+++ app/src/main/java/com/database/LitePalTest.java
@@ -1,11 +1,49 @@
 package com.database;
 
+import com.common.utils.LitePalUtil;
+import com.common.utils.LogUtil;
+
+import org.litepal.LitePal;
+
+import java.util.List;
+
 /**
  * Author: Pan
  * 2021/7/30
  * Description:
  */
+@SuppressWarnings("unused")
 public class LitePalTest {
 
+    public static void save() {
+        LitePalTestEntity last = LitePal.findLast(LitePalTestEntity.class);
+        if (last != null) {
+            LitePalTestEntity entity = new LitePalTestEntity();
+            entity.setId(last.getId() + 1);
+            entity.setName("我的ID是：" + entity.getId());
+        } else {
+            LogUtil.i("last为null");
+        }
+
+
+    }
+
+    public static void findAll() {
+        List<LitePalTestEntity> list = LitePalUtil.findAll(LitePalTestEntity.class);
+        int size = list.size();
+        for (int i = 0; i < size; i++) {
+            LitePalTestEntity entity = list.get(i);
+            LogUtil.d(entity.toString());
+        }
+    }
+
+    public static void findLast() {
+        LitePalTestEntity last = LitePalUtil.findLast(LitePalTestEntity.class);
+        if (last != null) {
+            LogUtil.d(last.toString());
+        } else {
+            LogUtil.i("last为null");
+        }
+    }
 
 }
--- app/src/main/java/com/database/LitePalTestEntity.java
+++ app/src/main/java/com/database/LitePalTestEntity.java
@@ -9,6 +9,30 @@ import org.litepal.crud.LitePalSupport;
  */
 public class LitePalTestEntity extends LitePalSupport {
     private long id;
+
     private String name;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
