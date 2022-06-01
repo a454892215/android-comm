31244bfa0ad121f91453ba0e05afb49ef6234576
LiuPan <1573802540@qq.com>
2022-06-01 08:31:28 +0800

    --

--- common/src/main/java/com/common/utils/CastUtil.java
+++ common/src/main/java/com/common/utils/CastUtil.java
@@ -3,7 +3,7 @@ package com.common.utils;
 /**
  * Author:  L
  * Description: No
- */                    android:layout_marginStart="5dp"
+ */
 
 public class CastUtil {
     @SuppressWarnings("unchecked")
