92e3dc7156d96decced4076d85ec39cefe18cfd7
Your Name <you@example.com>
2022-10-01 23:20:26 +0800

    -

---
+++ app/src/main/java/com/kotl/Note10.kt
@@ -0,0 +1,23 @@
+package com.kotl
+
+
+/**
+ * Author: L
+ * 2022/10/1
+ * Description:枚举类
+ *  1.枚举
+ *     01. 语法: enum class 类名{}
+ *     02. 对于数据类，定义在构造函数中的成员，会自动生成其解构声明
+ *  2. 运算符重载
+ *     01. 语法: operator fun 运算符对应的函数名(参数) = 表达式
+ */
+
+private enum class A10() {
+    Left,
+    Right,
+    Back,
+}
+
+fun main() {
+
+}
