daf6abe7982dfb7560748d03005f6ec6363b80b5
Your Name <you@example.com>
2022-10-02 22:30:09 +0800

    -

--- app/src/main/java/com/kotl/Note12_泛型.kt
+++ app/src/main/java/com/kotl/Note12_泛型.kt
@@ -8,10 +8,11 @@ package com.kotl
  *  1.泛型
  *     01.泛型类. 语法： class  className<T>(var p: T){}
  *     01.泛型函数. 语法：  fun <B> methodName(p: B)
- *  2.协变out
+ *  2.协变 out : 使子类泛型对象可以赋值给父类泛型对象
+ *  2.逆变 in : 使父类泛型对象可以赋值给子类泛型对象
  */
 
-interface Parent {}
+interface Parent
 interface Parent2 {
     fun getName(): String
 }
@@ -40,15 +41,9 @@ interface Consumer<in T> {
     // fun product(): T // 这里T做返回 T会报错
 }
 
-// 不加out/int 则T既能返回又能做函数参数？
-interface ProductionConsumer<T> {
-    fun product(): T
-    fun consume(item: T)
-}
 
 open class Food
 open class FastFood : Food()
-class Burger : FastFood()
 class FoodStore : Production<Food> {
     override fun product(): Food {
         return Food()
@@ -61,16 +56,24 @@ class FastFoodStore : Production<FastFood> {
     }
 }
 
-class BurgerStore : Production<Burger> {
-    override fun product(): Burger {
-        return Burger()
-    }
+open class Drink
+open class Water : Drink()
+
+class Boy : Consumer<Drink> {
+    override fun consume(item: Drink) {}
 }
 
+class Girl : Consumer<Water> {
+    override fun consume(item: Water) {}
+}
+
+
 fun main() {
-  var production1: Production<Food> = FoodStore()
-    // 因为 Production类添加了out协变，故可以通过编译
-  var production2: Production<Food> = FastFoodStore()
-  var production3: Production<Food> = BurgerStore()
+    // 1. out 协变示例： 使子类泛型对象可以赋值给父类泛型对象
+    var production2: Production<Food> = FastFoodStore()
+
+    // 2. in 逆变示例: 使父类泛型对象可以赋值给子类泛型对象
+    var consumer1: Consumer<Water> = Boy()
+    var consumer2: Consumer<Water> = Girl()
 
 }
