31b17c41d0f657496d0eccf46d2d2199ddb18722
Your Name <you@example.com>
2022-10-02 23:01:56 +0800

    -

--- app/src/main/java/com/kotl/Note12_泛型.kt
+++ app/src/main/java/com/kotl/Note12_泛型.kt
@@ -9,7 +9,8 @@ package com.kotl
  *     01.泛型类. 语法： class  className<T>(var p: T){}
  *     01.泛型函数. 语法：  fun <B> methodName(p: B)
  *  2.协变 out : 使子类泛型对象可以赋值给父类泛型对象
- *  2.逆变 in : 使父类泛型对象可以赋值给子类泛型对象
+ *  3.逆变 in : 使父类泛型对象可以赋值给子类泛型对象
+ *  4. reified 使泛型编译后不被擦除，使用示例：  inline fun<reified T> getName(){}
  */
 
 interface Parent
@@ -26,6 +27,11 @@ class A121<T : Parent>(var p: T) {
     // 函数上声明的泛型
     fun <B : Parent2> getA(p: B): String {
         return p.getName()
+    }
+
+    // reified 使用示例
+    inline fun<reified T> getName(){
+
     }
 }
 
