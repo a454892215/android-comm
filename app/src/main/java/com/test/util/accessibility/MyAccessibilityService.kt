8edd123d9f7abe6497e94771d7400f1e6877e327
henry <henry@gmail.com>
2023-01-25 21:01:14 +0800

    ok

--- app/src/main/java/com/test/util/accessibility/MyAccessibilityService.kt
+++ app/src/main/java/com/test/util/accessibility/MyAccessibilityService.kt
@@ -2,7 +2,9 @@ package com.test.util.accessibility
 
 import android.accessibilityservice.AccessibilityService
 import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityNodeInfo
 import com.test.util.utils.AppLog
+
 
 class MyAccessibilityService : AccessibilityService() {
 
@@ -11,18 +13,22 @@ class MyAccessibilityService : AccessibilityService() {
     override fun onServiceConnected() {
         super.onServiceConnected()
         AppLog.d("======onServiceConnected=========")
+        val mNodeInfo = this.rootInActiveWindow
+        val list: List<AccessibilityNodeInfo> = mNodeInfo.findAccessibilityNodeInfosByViewId("com.android.settings:id/force_stop_button")
+        for (i in list.indices) {
+            val node: AccessibilityNodeInfo = list[i]
+            node.performAction(AccessibilityNodeInfo.ACTION_CLICK)
+        }
+       // mNodeInfo.findAccessibilityNodeInfosByText("")
     }
 
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-        AppLog.d("======onAccessibilityEvent======${event.toString()}===")
-        val source = event?.source
-//        PerformAbility.findSoleId(ServiceConfig.service?.rootInActiveWindow,
-//            "vn.com.techcombank.bb.app:id/baseSearchInput"
-//        )
+        AppLog.d("======onAccessibilityEvent======${event.toString()}")
     }
 
     override fun onInterrupt() {
-        AppLog.d("======onInterrupt=========")
+        AppLog.d(" ====== onInterrupt ========= ")
     }
+
 }
