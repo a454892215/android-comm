789b051f42fc3acf887eb148bd9499d79dd6c72d
Your Name <1573802540@qq.com>
2023-01-29 23:00:13 +0800

    java-nodeloader

---
+++ app/src/main/java/com/test/util/accessibility/NewPageNodeLoader.java
@@ -0,0 +1,72 @@
+package com.test.util.accessibility;
+
+import android.os.SystemClock;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import com.test.util.utils.AppLog;
+
+/**
+ * 新页面节点加载器...
+ */
+public class NewPageNodeLoader {
+
+    public static final int perSleepTime = 400;
+    public static final int maxLoadTimes = 15;
+    private AccessibilityNodeInfo firstNode;
+    private AccessibilityNodeInfo tarNode;
+    private String id;
+    private String text;
+
+
+    public AccessibilityNodeInfo loadById(String id) {
+        this.id = id;
+        return load();
+    }
+
+    public AccessibilityNodeInfo loadByText(String text) {
+        this.text = text;
+        return load();
+    }
+
+    private AccessibilityNodeInfo load() {
+        for (int i = 0; i < maxLoadTimes; i++) {
+            if (i == 0) {
+                firstNode = getNode();
+            } else {
+                tarNode = getNode();
+            }
+            AppLog.INSTANCE.d(getLoadedNodeInfo() + " costTime:" + (i * perSleepTime));
+            if (tarNode != null) {
+                if (tarNode != firstNode) {
+                    return tarNode;
+                } else {
+                    AppLog.INSTANCE.e("异常firstNode和tarNode是同一个对象");
+                }
+            }
+            SystemClock.sleep(perSleepTime);
+        }
+        AppLog.INSTANCE.e("异常，没有找到目标节点：" + getLoadedNodeInfo() + " costTime:" + (maxLoadTimes * perSleepTime));
+        return null;
+    }
+
+    private String getLoadedNodeInfo() {
+        String firstNodeInfo = "firstNode:" + firstNode.getText() + " hashCode" + firstNode.hashCode();
+        String tarNodeInfo = "tarNode:" + tarNode.getText() + " tarNode" + tarNode.hashCode();
+        return firstNodeInfo + " " + tarNodeInfo;
+    }
+
+    public AccessibilityNodeInfo getNode() {
+        if (id != null) {
+            // TODO
+            // AccessibilityUtil.getNodeById(id);
+            return null;
+        } else if (text != null) {
+            // TODO
+            // AccessibilityUtil.getNodeByText(text);
+            return null;
+        } else {
+            AppLog.INSTANCE.e("id 和 text不能都是null");
+        }
+        return null;
+    }
+}
