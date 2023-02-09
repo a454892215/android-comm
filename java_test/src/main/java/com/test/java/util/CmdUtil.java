5789c837504d5044e93596023fa04dc43a23a46a
henry <henry@gmail.com>
2023-02-09 19:24:44 +0800

    -

--- java_test/src/main/java/com/test/java/util/CmdUtil.java
+++ java_test/src/main/java/com/test/java/util/CmdUtil.java
@@ -2,26 +2,32 @@ package com.test.java.util;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 class CmdUtil {
 
     static void startCmd(String cmd, File dir) {
-        StringBuilder sb = new StringBuilder();
         try {
             Process child = Runtime.getRuntime().exec(cmd, null, dir);
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(child.getInputStream()));
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                sb.append(line).append("\n");
-            }
-            bufferedReader.close();
+            String info = getInfo(child);
             child.waitFor();
             LogUtil.d("cmd:" + cmd);
-            LogUtil.d("======:" + "out:" + sb.toString() + "cmd finished");
+            LogUtil.d("======:" + "out:" + info + "cmd finished");
 
         } catch (Exception e) {
             LogUtil.e("======:" + e);
         }
+    }
+
+    private static String getInfo(Process process) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        bufferedReader.close();
+        return sb.toString();
     }
 }
