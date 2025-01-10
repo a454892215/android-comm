cc3706225f9684d69739c6e93594ddbb7e051fa7
Henry <llpph866@gmail.com>
2025-01-10 11:54:04 +0700

    yg

--- data_server/src/main/java/com/cand/data_server/CandleEntity.java
+++ data_server/src/main/java/com/cand/data_server/CandleEntity.java
@@ -15,10 +15,28 @@ public class CandleEntity {
         // 无参构造函数，JPA要求
     }
 
+    public CandleEntity(LocalDateTime timestamp, BigDecimal close, BigDecimal open, BigDecimal high, BigDecimal low, BigDecimal volume) {
+        this.timestamp = timestamp;
+        this.close = close;
+        this.open = open;
+        this.high = high;
+        this.low = low;
+        this.volume = volume;
+    }
+
+    public static CandleEntity createSimpleObj() {
+        CandleEntity candleEntity = new CandleEntity();
+        candleEntity.close = BigDecimal.valueOf(20);
+        candleEntity.open = BigDecimal.valueOf(30);
+        candleEntity.high = BigDecimal.valueOf(59);
+        candleEntity.low = BigDecimal.valueOf(10);
+        candleEntity.volume = BigDecimal.valueOf(30000);
+        return candleEntity;
+    }
+
     @Id
     @Column(name = "date", nullable = false, updatable = false)
     public LocalDateTime timestamp; // 使用日期作为主键
-
     @Column(name = "close", nullable = false, precision = 36, scale = 16)
     public BigDecimal close;
 
--- data_server/src/main/java/com/cand/data_server/RepositoryTest.java
+++ data_server/src/main/java/com/cand/data_server/RepositoryTest.java
@@ -1,18 +1,33 @@
 package com.cand.data_server;
 
+import static com.cand.data_server.H2TableGenerator.generateTable;
+import static com.cand.data_server.H2TableGenerator.printTableStructure;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class RepositoryTest {
+    public static String tableName = "candle_test";
 
-    public static void main(String[] args) throws SQLException {
-        Connection connect = H2DatabaseUtils.connect();
-        Repository repository = new Repository(connect);
-        repository.insertEntity(new CandleEntity(), "candle_test");
+    public static void main(String[] args) throws Exception {
+        testCreateTable();
+        testInsert();
     }
 
-    public static void testInsert(){
+    public static void testCreateTable() throws Exception {
+        // 示例：生成表
+        generateTable(CV.JDBC_URL, CandleEntity.class, tableName);
+        // 示例：打印表结构
+        printTableStructure(CV.JDBC_URL, tableName);
+    }
 
+    public static void testInsert() throws SQLException {
+        System.out.println("开始插入数据...");
+        Connection connect = H2DatabaseUtils.connect();
+        Repository repository = new Repository(connect);
+        CandleEntity candleEntity = CandleEntity.createSimpleObj();
+        repository.insertEntity(candleEntity, tableName);
+        System.out.println("插入数据成功..." + candleEntity.timestamp);
     }
 
     public static void testFind(){
