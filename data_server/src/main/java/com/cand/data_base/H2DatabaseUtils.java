4c643d45be027844c0f402ba906c72020013689c
Henry <llpph866@gmail.com>
2025-01-14 11:39:20 +0700

    yh

--- data_server/src/main/java/com/cand/data_base/H2DatabaseUtils.java
+++ data_server/src/main/java/com/cand/data_base/H2DatabaseUtils.java
@@ -16,11 +16,11 @@ public class H2DatabaseUtils {
 
     static {
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(CV.JDBC_URL); // 修改为你的数据库路径
-        config.setUsername("sa");
-        config.setPassword("");
-        config.setConnectionTimeout(3 * 1000); // 3秒
-        config.setMaximumPoolSize(10); // 最大连接数
+        config.setConnectionTimeout(5 * 1000); // 等待连接超时时间设为 5 秒
+        config.setMaximumPoolSize(50); // 最大连接池大小，建议从 50 开始，根据负载调整
+        config.setMinimumIdle(10); // 最小空闲连接数，确保低负载时也有可用连接
+        config.setIdleTimeout(10 * 1000); // 空闲连接超时时间，避免无用连接占用资源
+        config.setMaxLifetime(30 * 60 * 1000); // 连接最大存活时间，避免长期连接导致问题
         dataSource = new HikariDataSource(config);
     }
 
--- data_server/src/main/java/com/cand/network/AsyncUndertowServer.java
+++ data_server/src/main/java/com/cand/network/AsyncUndertowServer.java
@@ -2,29 +2,26 @@ package com.cand.network;
 
 import com.cand.util.LogUtil;
 
-import io.undertow.util.Headers;
 import io.undertow.Undertow;
+import io.undertow.util.Headers;
 
 // http://localhost:8080/?clientId=client1
-public class AsyncUndertowExample {
+public class AsyncUndertowServer {
 
 
     public static void main(String[] args) {
-        // 创建并启动 Undertow 服务器
+        startServer();
+    }
+
+    private static void startServer() {
         Undertow server = Undertow.builder()
-                .addHttpListener(8080, "localhost") // 监听本地 8080 端口
+                .addHttpListener(8080, "localhost")
                 .setHandler(exchange -> {
                     String path = exchange.getRequestPath();
-                    // 获取客户端请求参数
                     String clientId = exchange.getQueryParameters().get("clientId").getFirst();
                     exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-
-                    // 异步处理请求
                     exchange.dispatch(() -> {
                         try {
-                            // 模拟等待操作（如从数据库获取数据）
-                            //   simulateLongPoll(clientId);
-                            // 响应客户端
                             if ("/favicon.ico".equals(path)) {
                                 // 忽略 favicon.ico 请求或返回 404
                                 exchange.setStatusCode(404).getResponseSender().send("");
@@ -34,28 +31,15 @@ public class AsyncUndertowExample {
                             exchange.getResponseSender().send("Hello, " + clientId + "! Data received.");
                             LogUtil.d("响应成功发送end：" + clientId);
                         } catch (Exception e) {
+                            exchange.setStatusCode(500).getResponseSender().send("Internal Server Error");
                             LogUtil.d("响应异常：" + clientId);
                             e.printStackTrace();
-                            //  exchange.setStatusCode(500).getResponseSender().send("Internal Server Error");
                         }
                     });
                 }).build();
 
         server.start(); // 启动服务器
-        LogUtil.d("Async Undertow server started on http://localhost:8080");
+        LogUtil.d("启动服务器: http://localhost:8080");
     }
 
-    /**
-     * 模拟长轮询或等待操作
-     *
-     * @param clientId 客户端 ID
-     */
-    private static void simulateLongPoll(String clientId) {
-        try {
-            LogUtil.d("Waiting for data from client: " + clientId);
-            Thread.sleep(5000); // 模拟等待数据的操作
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 }
